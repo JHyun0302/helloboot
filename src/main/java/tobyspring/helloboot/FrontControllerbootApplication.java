700831b7746453e15e74b0003a308f087c22f010
JHyun0302 <wogustkfkd11@gmail.com>
2023-08-31 11:45:42 +0900

    :art: [Refactor] 프론트 컨트롤러 도입

---
+++ src/main/java/tobyspring/helloboot/FrontControllerbootApplication.java
@@ -0,0 +1,54 @@
+package tobyspring.helloboot;
+
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.server.WebServer;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * FrontController 적용
+ * - 여러 서블릿 등록되면서 중복 코드 및 공통화된 작업 해줄 컨트롤러 필요하게 됨
+ * ex. 인증 보안, 다국어 공통 기능
+ */
+public class FrontControllerbootApplication {
+
+	public static void main(String[] args) {
+		ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory(); //톰캣 서버를 만드는 복잡한 설정을 간단하게 제공
+		WebServer webServer = serverFactory.getWebServer(servletContext -> {
+			HelloController helloController = new HelloController(); //helloController를 사용하는 frontController 만들기 (helloController에 작업 위임)
+
+			servletContext.addServlet("frontcontroller", new HttpServlet() {
+				@Override
+				protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+					// 인증 보안, 다국어 공통 기능
+					if (req.getRequestURI().equals("/hello") && req.getMethod().equals(HttpMethod.GET.name())) {
+						String name = req.getParameter("name");
+
+						String ret = helloController.hello(name);
+
+						resp.setStatus(HttpStatus.OK.value());
+						resp.setHeader(HttpHeaders.CONTENT_TYPE, MediaType.TEXT_PLAIN_VALUE);
+						resp.getWriter().println(ret);
+					} else if (req.getRequestURI().equals("/user")) {
+						//
+					}else {
+						resp.setStatus(HttpStatus.NOT_FOUND.value());
+					}
+				}
+			}).addMapping("/*");
+
+		}); //톰캣외 다양한 서블릿 기술들 쓸 수 있게 추상화한 결과
+		webServer.start(); // 톰캣 서블릿 컨테이너 실행
+
+	}
+
+}
