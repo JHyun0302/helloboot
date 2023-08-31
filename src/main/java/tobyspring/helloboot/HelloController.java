0a066f66e5ca4d45659111fa234050b747ceb595
JHyun0302 <wogustkfkd11@gmail.com>
2023-08-31 19:40:29 +0900

    :sparkles: [Feat] 기본 예제 추가

--- src/main/java/tobyspring/helloboot/HelloController.java
+++ src/main/java/tobyspring/helloboot/HelloController.java
@@ -1,8 +1,24 @@
 package tobyspring.helloboot;
 
-public class HelloController {
+import org.springframework.web.bind.annotation.*;
 
+import java.util.Objects;
+
+/**
+ * Dependency Injection
+ */
+@RequestMapping("/hello")
+public class HelloController {
+    private final HelloService helloService;
+
+    public HelloController(HelloService helloService) {
+        this.helloService = helloService;
+    }
+
+    @GetMapping()
+    @ResponseBody
+//    @RequestMapping(value = "/hello", method = RequestMethod.GET)
     public String hello(String name) {
-        return "Hello " + name;
+        return helloService.sayHello(Objects.requireNonNull(name)); //name == null 이면 예외 던짐
     }
 }
