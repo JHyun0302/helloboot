87fac921676f9b764494eaea42b201c0684371d2
JHyun0302 <wogustkfkd11@gmail.com>
2023-09-01 19:09:03 +0900

    :sparkles: [Feat] 검증 로직 추가

--- src/main/java/tobyspring/helloboot/v8/HelloController.java
+++ src/main/java/tobyspring/helloboot/v8/HelloController.java
@@ -1,12 +1,8 @@
 package tobyspring.helloboot.v8;
 
-import org.springframework.context.ApplicationContext;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import tobyspring.helloboot.HelloService;
-
-import java.util.Objects;
 
 /**
  * @Component -> @MyCompoent -> @Controller -> @RestController
@@ -14,21 +10,18 @@ import java.util.Objects;
 @RestController
 public class HelloController {
     private final HelloService helloService;
-    private final ApplicationContext applicationContext;
 
-
-    public HelloController(HelloService helloService, ApplicationContext applicationContext) {
+    public HelloController(HelloService helloService) {
         this.helloService = helloService;
-        this.applicationContext = applicationContext;
-
-        System.out.println(applicationContext);
     }
 
     @GetMapping("/hello")
-    @ResponseBody
-//    @RequestMapping(value = "/hello", method = RequestMethod.GET)
     public String hello(String name) {
-        return helloService.sayHello(Objects.requireNonNull(name)); //name == null 이면 예외 던짐
+        if (name == null || name.trim().length() == 0) {
+            throw new IllegalArgumentException();
+        }
+        return helloService.sayHello(name);
+//        return helloService.sayHello(Objects.requireNonNull(name)); //name == null 이면 예외 던짐
     }
 
 }
