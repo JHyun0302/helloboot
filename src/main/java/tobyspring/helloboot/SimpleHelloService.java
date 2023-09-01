9ecf936f926c90c7f2b1690602e4a70757c5286a
JHyun0302 <wogustkfkd11@gmail.com>
2023-09-01 19:08:09 +0900

    :fire: [Remove] 필요 없는 파일 삭제

--- src/main/java/tobyspring/helloboot/SimpleHelloService.java
+++
@@ -1,8 +0,0 @@
-package tobyspring.helloboot;
-
-public class SimpleHelloService implements HelloService {
-    @Override
-    public String sayHello(String name) {
-        return "Hello " + name;
-    }
-}
